952c3588d12373673404306ed4fb4b33d6f8f32e
Rahul Saraf <rahuketu86@gmail.com>
2020-09-13 20:30:54 +0000

    Added code for extension installation

--- RemoteConnect/_nbdev.py
+++ RemoteConnect/_nbdev.py
@@ -7,6 +7,7 @@ index = {"IN_COLAB": "00_core.ipynb",
          "say_hello": "00_core.ipynb",
          "connect_to_ngrok": "00_core.ipynb",
          "RemoteCode": "00_core.ipynb",
+         "NOTEBOOK_EXTENSIONS": "00_core.ipynb",
          "RemoteJupyter": "00_core.ipynb",
          "start_code": "01_cli.ipynb",
          "start_jupyter": "01_cli.ipynb"}
