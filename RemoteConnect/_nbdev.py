6166afd6802a74920c4fa40f564b726a7cc5d9c7
Rahul Saraf <rahuketu86@gmail.com>
2020-09-13 17:46:56 +0000

    Enabled remote jupyter kernel

--- RemoteConnect/_nbdev.py
+++ RemoteConnect/_nbdev.py
@@ -7,7 +7,9 @@ index = {"IN_COLAB": "00_core.ipynb",
          "say_hello": "00_core.ipynb",
          "connect_to_ngrok": "00_core.ipynb",
          "RemoteCode": "00_core.ipynb",
-         "start_code": "01_cli.ipynb"}
+         "RemoteJupyter": "00_core.ipynb",
+         "start_code": "01_cli.ipynb",
+         "start_jupyter": "01_cli.ipynb"}
 
 modules = ["core.py",
            "cli.py"]
