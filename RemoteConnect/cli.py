b127109e3e84e344a2da5652c1505884c8af5131
Rahul Saraf <rahuketu86@gmail.com>
2020-09-19 15:11:32 +0000

    Added some modification of cli

--- RemoteConnect/cli.py
+++ RemoteConnect/cli.py
@@ -11,8 +11,7 @@ from .core import RemoteCode, RemoteJupyter, IN_COLAB, mount_drive
 def start_code(port:Param("Port to Start Code", type=int)=10000,
                password:Param("Password to Start Code", type=str)=None):
     "Starts Code Server"
-    if IN_COLAB:
-        mount_drive()
+    if IN_COLAB: mount_drive()
     print(RemoteCode(password=password, port=port))
 
 
@@ -20,6 +19,5 @@ def start_code(port:Param("Port to Start Code", type=int)=10000,
 @call_parse
 def start_jupyter(port:Param("Port to Start Code", type=int)=9000):
     "Starts Jupyter"
-    if IN_COLAB:
-        mount_drive()
+    if IN_COLAB: mount_drive()
     RemoteJupyter(port=port)
