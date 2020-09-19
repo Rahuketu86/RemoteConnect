12ff6fc68a4ef42bb98a1fb5dce675246f368e6a
Rahul Saraf <rahuketu86@gmail.com>
2020-09-19 13:58:21 +0000

    Changed order of mounting drive

--- RemoteConnect/cli.py
+++ RemoteConnect/cli.py
@@ -4,13 +4,15 @@ __all__ = ['start_code', 'start_jupyter']
 
 # Cell
 from fastscript import call_parse, Param, bool_arg
-from .core import RemoteCode, RemoteJupyter
+from .core import RemoteCode, RemoteJupyter, IN_COLAB, mount_drive
 
 # Cell
 @call_parse
 def start_code(port:Param("Port to Start Code", type=int)=10000,
                password:Param("Password to Start Code", type=str)=None):
     "Starts Code Server"
+    if IN_COLAB:
+        mount_drive()
     print(RemoteCode(password=password, port=port))
 
 
@@ -18,4 +20,6 @@ def start_code(port:Param("Port to Start Code", type=int)=10000,
 @call_parse
 def start_jupyter(port:Param("Port to Start Code", type=int)=9000):
     "Starts Jupyter"
+    if IN_COLAB:
+        mount_drive()
     RemoteJupyter(port=port)
