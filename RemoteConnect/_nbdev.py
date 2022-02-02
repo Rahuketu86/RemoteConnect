63a86369bfc587c23dd00f6146609db868624d13
Rahul Saraf-halliburton.com <rahul.saraf@DSW210826192950.CZ1CG1.PRI>
2022-02-02 19:57:36 +0000

    Clean up

--- RemoteConnect/_nbdev.py
+++ RemoteConnect/_nbdev.py
@@ -2,8 +2,8 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"IN_COLAB": "00_core.ipynb",
-         "say_hello": "00_core.ipynb",
+index = {"say_hello": "00_core.ipynb",
+         "IN_COLAB": "00_core.ipynb",
          "connect_to_telebit": "00_core.ipynb",
          "connect_to_ngrok": "00_core.ipynb",
          "execute_cmd": "00_core.ipynb",
