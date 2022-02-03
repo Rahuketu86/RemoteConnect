26b76e738c1a7502207a1e0d81ba0b617417a78b
Rahul Saraf-halliburton.com <rahul.saraf@DSW210826192950.CZ1CG1.PRI>
2022-02-03 13:40:37 +0000

    Fixing localtunnel on colab

--- RemoteConnect/core.py
+++ RemoteConnect/core.py
@@ -54,6 +54,7 @@ def connect_to_localtunel(port):
         #subprocess.run(f"lt --port {port} --subdomain nbrahuketu>> {url_folder}/url.txt 2>&1 &", stderr=subprocess.STDOUT, shell=True)
         execute_cmd(f"lt --port {port} --subdomain nbrahuketu>> {url_folder}/url.txt 2>&1 &")
         time.sleep(1)
+        print("Reading Generated url file")
         s = pathlib.Path(f"{url_folder}/url.txt").open().read()
         print(s)
         url = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', str(s))
