1b76cadcbea6318ce09e2bb6ed1a5e8a4df00413
Rahul Saraf-halliburton.com <rahul.saraf@DSW210826192950.CZ1CG1.PRI>
2022-02-06 15:22:53 +0000

    Updated colab script

--- update_colab.sh
+++ update_colab.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip install -U pip install -U git+https://github.com/fastai/fastdownload.git
+pip install -U git+https://github.com/fastai/fastdownload.git
 pip3 install pickle5
 sudo apt-get install htop
 npm install -g http-server
